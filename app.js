8605aad9803578b9471e1387a69a7e7d5a9e0f26
Chris Burke <burkech@umail.iu.edu>
2017-06-29 13:04:40 -0500

    Baywatch: Added functioning delete button and favorite background change.

--- app.js
+++ app.js
@@ -15,12 +15,54 @@ const app = {
   renderListItem(flick) {
     const item = document.createElement('li')
     const btn = document.createElement('button')
+    const delbtn = document.createElement('button')
+    const upbtn = document.createElement('button')
+
 
     item.textContent = flick.name
+   
     btn.textContent = "Favorite"
     btn.className += "primary button"
+
+    delbtn.textContent = "Delete"
+    delbtn.className += 'alert button'
+
+    // upbtn.textContent = "Up"
+    // upbtn.className += 'info button'
+    
+
+    btn.addEventListener('click', function () {
+    
+        if (this.parentElement.style.backgroundColor === 'yellow') {
+            this.parentElement.style.backgroundColor = ''
+        } else {
+            this.parentElement.style.backgroundColor = 'yellow'
+        }
+
+    })
+
+    delbtn.addEventListener('click', function (max) {
+        if (app.max >= 0) {
+            item.remove()
+        } else {
+            
+        }
+    })
+
+    // upbtn.addEventListener('click', function () {
+    //     if (app.max >= 0) {
+    //         flick.max - 1
+    //     } else {
+
+    //     }
+    // })
+
+    
     item.appendChild(btn)
+    item.appendChild(delbtn)
+    //item.appendChild(upbtn)
     return item
+
   },
 
   handleSubmit(ev) {
